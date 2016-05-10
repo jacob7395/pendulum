2097d566463e6814d5cabaca74202c7d0ee73434
Jacob_Mac <jacob7395@gmail.com>
2016-05-10 10:46:02 +0100

    PID Code integrated

--- Matlab/UDP.m
+++ Matlab/UDP.m
@@ -18,7 +18,7 @@ end
 % these our our ip and port
 % port must be > 1024
 set(obj1, 'LocalHost', Local_IP);
-set(obj1, 'LocalPort', 63239);
+set(obj1, 'LocalPort', 63242);
 set(obj1, 'LocalPortMode', 'manual');
 
 % Connect to instrument object
@@ -64,53 +64,53 @@ while count <= loop
     % wait for udp packet
     % default time = 10 sec but can be changed
     s = fscanf(obj1);
-    
-    if length(s > 1)
+    %check a packet has been resived
+    if length(s > 1) && size(str2num(s),2) == 5
         data_buffer = str2num(s);
-        if     data(size(data,1),1) < data_buffer(1,1) && size(str2num(s),2) == 5
+        if     data(size(data,1),1) < data_buffer(1,1)
             data = [data;str2num(s)];
-        elseif size(str2num(s),2) == 5
+                %plot scroling graph
+                while size(data,1)-300 > n
+                    axis(ax1,[data(n,1),1+data(n+300,1),-1500,1500])
+                    axis(ax3,[data(n,1),1+data(n+300,1),-180,180])
+                    axis(ax2,[data(n,1),1+data(n+300,1),-1.0,1.0])
+
+                    Plot_One    = plot(ax1,data(n:n+300,1),  data(n:n+300,2), 'r');
+                    Plot_Two    = plot(ax2,data(n:n+300,1),  data(n:n+300,3), 'g');
+                    Plot_Three  = plot(ax3,data(n:n+300,1),  data(n:n+300,4), 'b');
+
+                    n = n + 1;
+                    pause(0.00001)
+
+                    delete(Plot_One)
+                    delete(Plot_Two)
+                    delete(Plot_Three)
+                end
+                %plot non scroling graph
+                if size(data,1)-300 < n
+                    Data_Size = size(data,1);
+
+                    axis(ax1,[data(n,1), data(n,1) + 20,-1500,1500])
+                    axis(ax3,[data(n,1), data(n,1) + 20,-180 ,180])
+                    axis(ax2,[data(n,1), data(n,1) + 20,-1.0 ,1.0])
+
+                    Plot_One    = plot(ax1,data(2:Data_Size,1),  data(2:Data_Size,2), 'r');
+                    Plot_Two    = plot(ax2,data(2:Data_Size,1),  data(2:Data_Size,3), 'g');
+                    Plot_Three  = plot(ax3,data(2:Data_Size,1),  data(2:Data_Size,4), 'b');
+
+                    pause(0.00001)
+
+                    delete(Plot_One)
+                    delete(Plot_Two)
+                    delete(Plot_Three)
+                end
+        else
             n = 1;
             data = [0,0,0,0,0];
             data = [data;str2num(s)];
         end
     else
-        disp('No Coms')
-    end
-    
-    while size(data,1)-300 > n
-        axis(ax1,[data(n,1),1+data(n+300,1),-1500,1500])
-        axis(ax3,[data(n,1),1+data(n+300,1),-180,180])
-        axis(ax2,[data(n,1),1+data(n+300,1),-1.0,1.0])
-
-        Plot_One    = plot(ax1,data(n:n+300,1),  data(n:n+300,2), 'r');
-        Plot_Two    = plot(ax2,data(n:n+300,1),  data(n:n+300,3), 'g');
-        Plot_Three  = plot(ax3,data(n:n+300,1),  data(n:n+300,4), 'b');
-
-        n = n + 1;
-        pause(0.00001)
-
-        delete(Plot_One)
-        delete(Plot_Two)
-        delete(Plot_Three)
-    end
-    
-    if size(data,1)-300 < n
-        Data_Size = size(data,1);
-
-        axis(ax1,[data(n,1), data(n,1) + 20,-1500,1500])
-        axis(ax3,[data(n,1), data(n,1) + 20,-180 ,180])
-        axis(ax2,[data(n,1), data(n,1) + 20,-1.0 ,1.0])
-
-        Plot_One    = plot(ax1,data(2:Data_Size,1),  data(2:Data_Size,2), 'r');
-        Plot_Two    = plot(ax2,data(2:Data_Size,1),  data(2:Data_Size,3), 'g');
-        Plot_Three  = plot(ax3,data(2:Data_Size,1),  data(2:Data_Size,4), 'b');
-
-        pause(0.00001)
-
-        delete(Plot_One)
-        delete(Plot_Two)
-        delete(Plot_Three)
+        disp('Bad Coms')
     end
  
     count = count + 1;
--- PI/UDP.h
+++
@@ -1,6 +0,0 @@
-#ifndef UDP_H_INCLUDED
-#define UDP_H_INCLUDED
-
-
-
-#endif // UDP_H_INCLUDED
